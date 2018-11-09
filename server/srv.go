3f56af99b244513ec8ec2cabc8caa30139c4fa6d
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-11-08 21:15:30 -0500

    updated srv.go for LogUserIn

--- server/srv.go
+++ server/srv.go
@@ -9,5 +9,8 @@ func main() {
 	http.HandleFunc("/newUnauthorizedSession", businesslogic.NewUnauthorizedSessionHandler)
 	http.HandleFunc("/newAuthorizedSession", businesslogic.NewAuthorizedSessionHandler)
 	http.HandleFunc("/new_user", businesslogic.NewUserHandler)
+  http.HandleFunc("/Login", businesslogic.LogUserIn)
+
 	http.ListenAndServe(":8000", nil);
+  
 }
