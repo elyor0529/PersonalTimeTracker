c641a2a81a355359af89508f3b3fd8e0f7e1b025
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-11-09 23:40:54 -0500

    added gui to client code

---
+++ client/TimeTracker/TimeTracker/Login.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timetracker
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+             string email;
+             string password;
+            if (txtEmail.Text == "" || txtPassword.Text == "") {
+
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            email = txtEmail.Text;
+            password = txtPassword.Text;
+            LoginData u = new LoginData()
+            {
+                Email = email,
+                UserName = password
+            };
+            ServerProxy view = new ServerProxy();
+
+            await view.sendRequest();
+
+            await view.postLogin(u);
+
+            
+        }
+
+        /*private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+
+        }*/
+
+
+    }
+}
--- client/TimeTracker/TimeTracker/Program.cs
+++ client/TimeTracker/TimeTracker/Program.cs
@@ -1,42 +1,22 @@
 ﻿using System;
-using System.IO;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Runtime.Serialization.Json;
-
+using System.Windows.Forms;
 
 namespace timetracker
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
-
-
-            ServerProxy s = new ServerProxy();
-            LoginData j = new LoginData
-            {
-                UserName = "Jalpesh",
-                Email = "Vadgama",
-                SessionKey = "999"
-            };
-            s.sendRequest().Wait();
-            s.postLogin(j).Wait();
-            /*System.IO.MemoryStream stream = new System.IO.MemoryStream();
-            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Login));
-            ser.WriteObject(stream, j);
-            byte[] b = stream.ToArray();
-
-            stream.Position = 0;
-            StreamReader sr = new StreamReader(stream);
-            string jason = sr.ReadToEnd();*/
-            
-            
-            
-           
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
         }
     }
 }
