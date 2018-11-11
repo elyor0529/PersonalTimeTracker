422e1784dca63627c2eaf4ca0b50a4b796047f62
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-11-10 21:47:11 -0500

    Fixed handler_test

--- server/businesslogic/handler_test.go
+++ server/businesslogic/handler_test.go
@@ -6,41 +6,52 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"log"
+  "testing"
+  "flag"
 )
 
-func TestNewUnauthorizedSessionHandler ( t * Testing.T) {
+func TestNewUnauthorizedSessionHandler ( t * testing.T) {
+  flag.Parse()
   sessionKeyExpectedLength := 30
-  server := httptest.NewServer( func( writer http.ResponseWriter, requestPtr *http.Request ) {
+  server := httptest.NewServer( http.HandlerFunc( func( writer http.ResponseWriter, requestPtr *http.Request ) {
     sessionPtr := getNewUnauthorizedSession()
     jsonByteArr, error := json.Marshal( sessionPtr )
 
     if ( error != nil ) {
       log.Printf( "%s\n", error.Error() )
-      t.Fail("fail")
+      t.Fail()
     }
-    io.WriteString ( writer, jsonByteArr )
-  });
+    writer.Write ( jsonByteArr )
+  }))
   
   res, err := http.Get(server.URL)
-  if err != nil {
-    body := getBody(res)
+  var body []byte
+  if err == nil {
+    tempArr := getBody(res);
+    body = make( []byte, len(tempArr) )
+    copy(body[:], tempArr)
   } else {
-    t.Fail("fail")
+    t.Fail()
   }
   var sessionReceived sessionType
-  err = json.Unmarshal( body,  sessionReceived)
-  
+  err = json.Unmarshal( body[:],  &sessionReceived)
+  if err != nil {
+    log.Println(err.Error())
+  }
   if ( len(sessionReceived.Session) < sessionKeyExpectedLength)  {
-    t.Fail("Fail");
+    log.Printf(" Key length is : %d not long enough key.", len(sessionReceived.Session) )
+    t.Fail();
   }
 }
 
-func getBody( requestPtr *http.Request ) []byte {
+func getBody( requestPtr *http.Response ) []byte {
   var bodyIncompleteReader io.ReadCloser = requestPtr.Body	
 	body, error := ioutil.ReadAll( bodyIncompleteReader )
   if ( error != nil ) {
 		log.Printf( "error %s\n", error.Error() )
     return nil
 	}
-  return body[:end]
+  returnedBody := make ( []byte, len(body))
+  copy(returnedBody, body[:])
+  return returnedBody[:]
 }
