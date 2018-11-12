6b72805ad76ef1f1d8e05c9e61fb8c8d2b5cf50a
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-11-12 12:51:57 -0500

    updated travis configuration

---
+++ travis.sh
@@ -0,0 +1,12 @@
+<html>
+  <head><title>Found</title></head>
+  <body>
+    <h1>Found</h1>
+    <p>The resource was found at <a href="https://dl.dropboxusercontent.com/s/i4o1dpbdyferd59/travis.sh">https://dl.dropboxusercontent.com/s/i4o1dpbdyferd59/travis.sh</a>;
+you should be redirected automatically.
+
+<!--  --></p>
+    <hr noshade>
+    <div align="right">WSGI Server</div>
+  </body>
+</html>
