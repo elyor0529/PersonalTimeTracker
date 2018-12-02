4f9739e87a571906bdc58e024d3d381a56a9aeb7
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-12-02 11:24:29 -0500

    Added RecoverPassword feature to server side.

--- server/businesslogic/Credentials.go
+++ server/businesslogic/Credentials.go
@@ -5,6 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"net/smtp"
+	"os"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 func LogUserIn(body []byte) (*LoginRequestResult, error, string) {
@@ -43,9 +48,8 @@ func CreateAccount(body []byte) (*CreateAccountRequestResult, error, string) {
 			SessionKey:          request.SessionKey,
 		}, err, ""
 	}
-	var space string = ""
 
-	account := datastore.MakeAccount(&request.Email, request.TimeZone, &request.Password, &space, &space, &space)
+	account := datastore.MakeAccount(&request.Email, request.TimeZone, &request.Password, &request.FirstName, &request.MiddleName, &request.LastName)
 	exists := accountExists(account)
 	if exists == true {
 		return &CreateAccountRequestResult{
@@ -59,6 +63,77 @@ func CreateAccount(body []byte) (*CreateAccountRequestResult, error, string) {
 			SessionKey:          "",
 		}, nil, request.Email
 	}
+}
+
+
+func RecoverPassword(body []byte) (*UpdatePasswordRequestResult, error){
+	var request UpdatePasswordRequest
+	err := json.Unmarshal(body[:], &request)
+	if err != nil {
+		log.Println(err.Error())
+		return &UpdatePasswordRequestResult{
+			UpdatePasswordResult: "Incompatible JSON request structure.",
+			SessionKey:          request.SessionKey,
+		}, err
+	} else { 
+			newPassword := generateRandomPassword()
+			
+			account := new (datastore.Account)
+			datastore.SetEmail( account, request.Email )
+			
+			if accountExists(account) == false {
+				return &UpdatePasswordRequestResult{
+					UpdatePasswordResult: "This email account does not exist.",
+					SessionKey:          request.SessionKey,
+				}, errors.New("This email account does not exist.")
+			}
+			databaseRecoverAccountError := datastore.RecoverPassword(account, newPassword)
+			if databaseRecoverAccountError != nil {
+				return &UpdatePasswordRequestResult{
+					UpdatePasswordResult: "Failure",
+					SessionKey:          request.SessionKey,
+				}, databaseRecoverAccountError
+			}
+			smtpError := sendUpdatePasswordEmail(request.Email, newPassword)
+			if smtpError == nil {
+				return &UpdatePasswordRequestResult{
+					UpdatePasswordResult: "Success",
+					SessionKey:          request.SessionKey,
+				}, smtpError
+			} else {
+				return &UpdatePasswordRequestResult{
+					UpdatePasswordResult: "Internal server error while sending new password to the email.",
+					SessionKey:          request.SessionKey,
+				}, smtpError
+			}
+	}
+}
+
+
+func sendUpdatePasswordEmail(recipientEmail string, newPassword string ) (error) {
+	from := os.Getenv("acs_SendingEmail")
+	pass := os.Getenv("acs_SendingEmailPassword")
+	to := recipientEmail
+	body := "Your new password is " + newPassword
+	msg := "From: " + from + "\r\n" +
+					"To: " + to + "\r\n" +
+					"Subject: New Password " + newPassword + "\r\n\r\n" +
+					body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Printf("smtp error: %s", err)
+	}
+	return err
+}
+
+func generateRandomPassword() (string){
+	var maxNumber int = 10000
+	rand.Seed(time.Now().UnixNano())
+	return strconv.FormatInt(int64(rand.Intn(maxNumber)), 10)
 }
 
 func accountExists(account *datastore.Account) bool {
@@ -78,3 +153,5 @@ func accountValid(account *datastore.Account) bool {
 		return false
 	}
 }
+
+
