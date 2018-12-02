05ddf505433e92588154ef95a6dd4057a26978a4
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-12-02 11:20:26 -0500

    Implemented GetTasksSharedWithMe feature

---
+++ client/TimeTracker/TimeTracker/GetTasksSharedWithMeData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public class GetTasksSharedWithMeData
+    {
+        [DataMember]
+        public string SessionKey;
+
+        private static DataContractJsonSerializer serializerCreateAccountData =
+           new DataContractJsonSerializer(typeof(GetTasksSharedWithMeData));
+        public System.IO.MemoryStream GetMemoryStream()
+        {
+            var createAccountSendingStream = new System.IO.MemoryStream();
+            serializerCreateAccountData.WriteObject(createAccountSendingStream, this);
+            return createAccountSendingStream;
+        }
+    }
+}
---
+++ client/TimeTracker/TimeTracker/GetTasksSharedWithMeResult.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public class GetTasksSharedWithMeResult
+    {
+        [DataMember]
+        public string GetAllSharedTasksResult;
+        [DataMember]
+        public SharedTask[] SharedTaskList;
+
+        private static DataContractJsonSerializer deserializerRetrieveTaskResultType =
+                new DataContractJsonSerializer(typeof(GetTasksSharedWithMeResult));
+        public static GetTasksSharedWithMeResult ReadFromStream(System.IO.Stream stream)
+        {
+            GetTasksSharedWithMeResult result = (GetTasksSharedWithMeResult)deserializerRetrieveTaskResultType.ReadObject(stream);
+            for (int i = 0; i < result.SharedTaskList.Length; i++)
+            {
+                result.SharedTaskList[i].parseDate();
+            }
+            return result;
+        }
+    }
+
+    [DataContract]
+    public class SharedTask
+    {
+        [DataMember]
+        public string TaskName;
+
+        [DataMember]
+        public double TimeSpent;
+
+        [DataMember]
+        public string TaskDate;
+
+        [DataMember]
+        public string EmailFrom;
+
+        public DateTime TaskDateTime;
+
+        public void parseDate()
+        {
+            TaskDateTime = DateTime.ParseExact(TaskDate, "yyyy-MM-dd'T'HH:mm:ss.FFFFFFF'Z'", CultureInfo.GetCultureInfo("en-US").DateTimeFormat);
+        }
+
+        public DateTime getDateTime()
+        {
+            return TaskDateTime;
+        }
+
+        public override string ToString()
+        {
+            return TaskName;
+        }
+    }
+}
