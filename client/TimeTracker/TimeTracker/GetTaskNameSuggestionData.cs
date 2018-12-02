d894e298b5bb29c3e91e7bc88745082e03b733e2
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-12-02 13:04:59 -0500

    Added JSON C# Data to client code

---
+++ client/TimeTracker/TimeTracker/GetTaskNameSuggestionData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public class GetTaskNameSuggestionData
+    {
+        [DataMember]
+        public string SessionKey;
+
+        private static DataContractJsonSerializer serializerTaskData =
+            new DataContractJsonSerializer(typeof(GetTaskNameSuggestionData));
+
+        public System.IO.MemoryStream GetMemoryStream()
+        {
+            var retrieveTaskStream = new System.IO.MemoryStream();
+            serializerTaskData.WriteObject(retrieveTaskStream, this);
+            return retrieveTaskStream;
+        }
+    }
+}
---
+++ client/TimeTracker/TimeTracker/GetTaskNameSuggestionResultType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Globalization;
+using System.IO;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public sealed class GetTaskNameSuggestionResultType
+    {
+        [DataMember]
+        public string GetTaskNameSuggestionResult;
+        [DataMember]
+        public string[] TaskNames;
+
+        private static DataContractJsonSerializer deserializerRetrieveTaskResultType =
+                new DataContractJsonSerializer(typeof(GetTaskNameSuggestionResultType));
+        public static GetTaskNameSuggestionResultType ReadFromStream(System.IO.Stream stream)
+        {
+            GetTaskNameSuggestionResultType result = (GetTaskNameSuggestionResultType)deserializerRetrieveTaskResultType.ReadObject(stream);
+            return result;
+        }
+
+        public bool IsSuccess()
+        {
+            return GetTaskNameSuggestionResult.Equals("Success");
+        }
+    }
+}
+    
