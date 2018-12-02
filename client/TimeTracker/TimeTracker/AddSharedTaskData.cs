887d80c850c21121e750755995bc0dfe4347208b
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-12-02 11:20:47 -0500

    Implemented AddSharedTask feature

---
+++ client/TimeTracker/TimeTracker/AddSharedTaskData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public class AddSharedTaskData
+    {
+        [DataMember]
+        public string TaskName;
+
+
+        [DataMember]
+        public string TaskDate;
+
+
+        private DateTime taskDateTime;
+        public DateTime TaskDateTime {
+        get{
+                return taskDateTime;
+        }
+        set {
+                TaskDate = XmlConvert.ToString(value, XmlDateTimeSerializationMode.Utc);
+                taskDateTime = value;
+            }
+        }
+
+
+
+        [DataMember]
+        public double TaskTimeSpent;
+
+        [DataMember]
+        public string EmailTo;
+
+        [DataMember]
+        public string SessionKey;
+
+        private static DataContractJsonSerializer serializerAddSharedTaskData =
+           new DataContractJsonSerializer(typeof(AddSharedTaskData));
+        public System.IO.MemoryStream GetMemoryStream()
+        {
+            var addSharedTaskDataSendingStream = new System.IO.MemoryStream();
+            serializerAddSharedTaskData.WriteObject(addSharedTaskDataSendingStream, this);
+            return addSharedTaskDataSendingStream;
+        }
+    }
+}
---
+++ client/TimeTracker/TimeTracker/AddSharedTaskResultType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public class AddSharedTaskResultType
+    {
+        [DataMember]
+        public string AddSharedTaskResult;
+
+        [DataMember]
+        public string SessionKey;
+
+        private static DataContractJsonSerializer deserializerAddRequestResultType =
+                    new DataContractJsonSerializer(typeof(AddSharedTaskResultType));
+
+        public static AddSharedTaskResultType ReadFromStream(System.IO.Stream stream)
+        {
+            return (AddSharedTaskResultType)deserializerAddRequestResultType.ReadObject(stream);
+        }
+    }
+}
