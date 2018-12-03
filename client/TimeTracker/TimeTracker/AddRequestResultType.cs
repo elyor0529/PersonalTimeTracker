c3dcabfbf255cbff0168b211ae136264c8f99d68
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-12-03 04:20:54 -0500

    Added AddRequestResultType.cs for client build

---
+++ client/TimeTracker/TimeTracker/AddRequestResultType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace TimeTracker
+{
+    [DataContract]
+    public sealed class AddRequestResult
+    {
+        public string AddResult { get; set; }
+    
+        private static DataContractJsonSerializer deserializerAddRequestResultType =
+                new DataContractJsonSerializer(typeof(AddRequestResult));
+
+        public static AddRequestResult ReadFromStream(System.IO.Stream stream)
+        {
+        return (AddRequestResult)deserializerAddRequestResultType.ReadObject(stream);
+        }
+
+        public bool IsSuccess()
+        {
+        return AddResult.Equals("Success");
+        }
+}
+}
